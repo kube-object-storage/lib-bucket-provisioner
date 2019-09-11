89f6ca79acf7ddd20f4f9849f5df433a3040bb3c
Jon Cope <copejon@users.noreply.github.com>
2019-09-11 12:53:35 -0500

    Custom labels (#159)
    * enable provisioner defined labels for manage resources
    
    reformatted function calls for readability
    
    Signed-off-by: Jon Cope <jcope@redhat.com>
    
    * fixed some log lines
    
    Signed-off-by: Jon Cope <jcope@redhat.com>

--- pkg/provisioner/controller.go
+++ pkg/provisioner/controller.go
@@ -40,6 +40,7 @@ import (
 
 type controller interface {
 	Start(<-chan struct{}) error
+	SetLabels(map[string]string)
 }
 
 // Provisioner is a CRD Controller responsible for executing the Reconcile() function in response to OB and OBC events.
@@ -52,6 +53,8 @@ type Controller struct {
 	obcHasSynced cache.InformerSynced
 	obHasSynced  cache.InformerSynced
 	queue        workqueue.RateLimitingInterface
+
+	provisionerLabels map[string]string
 
 	provisioner     api.Provisioner
 	provisionerName string
@@ -101,16 +104,6 @@ func NewController(provisionerName string, provisioner api.Provisioner, clientse
 	return ctrl
 }
 
-func (c *Controller) enqueueOBC(obj interface{}) {
-	var key string
-	var err error
-	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
-		utilruntime.HandleError(err)
-		return
-	}
-	c.queue.AddRateLimited(key)
-}
-
 func (c *Controller) Start(stopCh <-chan struct{}) error {
 	defer utilruntime.HandleCrash()
 	defer c.queue.ShutDown()
@@ -122,6 +115,20 @@ func (c *Controller) Start(stopCh <-chan struct{}) error {
 
 	<-stopCh
 	return nil
+}
+
+func (c *Controller) SetLabels(labels map[string]string) {
+	c.provisionerLabels = labels
+}
+
+func (c *Controller) enqueueOBC(obj interface{}) {
+	var key string
+	var err error
+	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
+		utilruntime.HandleError(err)
+		return
+	}
+	c.queue.AddRateLimited(key)
 }
 
 func (c *Controller) runWorker() {
@@ -181,7 +188,7 @@ func (c *Controller) processNextItemInQueue() bool {
 // Reconcile implements the Reconciler interface. This function contains the business logic
 // of the OBC Controller.
 // Note: the obc obtained from the key is not expected to be nil. In other words, this func is
-//   not called when informers detect an object is missing and trigger a formal delete event. 
+//   not called when informers detect an object is missing and trigger a formal delete event.
 //   Instead, delete is indicated by the deletionTimestamp being non-nil on an update event.
 func (c *Controller) syncHandler(key string) error {
 
@@ -242,15 +249,20 @@ func (c *Controller) handleProvisionClaim(key string, obc *v1alpha1.ObjectBucket
 		secret    *corev1.Secret
 		configMap *corev1.ConfigMap
 	)
-	obcNsName := obc.Namespace + "/" + obc.Name
 
 	// first step is to update the OBC's status to pending
-	if obc, err = updateObjectBucketClaimPhase(c.libClientset, obc, v1alpha1.ObjectBucketClaimStatusPhasePending, defaultRetryBaseInterval, defaultRetryTimeout); err != nil {
-		return fmt.Errorf("error updating OBC %q's status to %q: %v", obcNsName, v1alpha1.ObjectBucketClaimStatusPhasePending, err)
+	obc, err = updateObjectBucketClaimPhase(
+		c.libClientset,
+		obc,
+		v1alpha1.ObjectBucketClaimStatusPhasePending,
+		defaultRetryBaseInterval,
+		defaultRetryTimeout)
+	if err != nil {
+		return fmt.Errorf("error updating OBC status to %q: %v", v1alpha1.ObjectBucketClaimStatusPhasePending, err)
 	}
 
 	// set finalizer in OBC so that resources can be cleaned up when the obc is deleted
-	if err = c.protectOBC(obc); err != nil {
+	if err = c.setOBCMetaFields(obc); err != nil {
 		return err
 	}
 
@@ -319,11 +331,25 @@ func (c *Controller) handleProvisionClaim(key string, obc *v1alpha1.ObjectBucket
 	}
 
 	// create Secret and ConfigMap
-	if secret, err = createSecret(obc, ob.Spec.Authentication, c.clientset, defaultRetryBaseInterval, defaultRetryTimeout); err != nil {
-		return fmt.Errorf("error creating secret for OBC %q: %v", obcNsName, err)
+	secret, err = createSecret(
+		obc,
+		ob.Spec.Authentication,
+		c.provisionerLabels,
+		c.clientset,
+		defaultRetryBaseInterval,
+		defaultRetryTimeout)
+	if err != nil {
+		return fmt.Errorf("error creating secret for OBC: %v", err)
 	}
-	if configMap, err = createConfigMap(obc, ob.Spec.Endpoint, c.clientset, defaultRetryBaseInterval, defaultRetryTimeout); err != nil {
-		return fmt.Errorf("error creating configmap for OBC %q: %v", obcNsName, err)
+	configMap, err = createConfigMap(
+		obc,
+		ob.Spec.Endpoint,
+		c.provisionerLabels,
+		c.clientset,
+		defaultRetryBaseInterval,
+		defaultRetryTimeout)
+	if err != nil {
+		return fmt.Errorf("error creating configmap for OBC: %v", err)
 	}
 
 	// Create OB
@@ -334,22 +360,45 @@ func (c *Controller) handleProvisionClaim(key string, obc *v1alpha1.ObjectBucket
 	ob.Spec.ClaimRef, err = claimRefForKey(key, c.libClientset)
 	ob.Spec.ReclaimPolicy = options.ReclaimPolicy
 	ob.SetFinalizers([]string{finalizer})
+	ob.SetLabels(c.provisionerLabels)
 
-	if ob, err = createObjectBucket(ob, c.libClientset, defaultRetryBaseInterval, defaultRetryTimeout); err != nil {
+	ob, err = createObjectBucket(
+		ob,
+		c.libClientset,
+		defaultRetryBaseInterval,
+		defaultRetryTimeout)
+	if err != nil {
 		return fmt.Errorf("error creating OB %q: %v", ob.Name, err)
 	}
-	if ob, err = updateObjectBucketPhase(c.libClientset, ob, v1alpha1.ObjectBucketStatusPhaseBound, defaultRetryBaseInterval, defaultRetryTimeout); err != nil {
-		return fmt.Errorf("error updating OB %q's status to %q:", ob.Name, v1alpha1.ObjectBucketStatusPhaseBound, err)
+	ob, err = updateObjectBucketPhase(
+		c.libClientset,
+		ob,
+		v1alpha1.ObjectBucketStatusPhaseBound,
+		defaultRetryBaseInterval,
+		defaultRetryTimeout)
+	if err != nil {
+		return fmt.Errorf("error updating OB %q's status to %q: %v", ob.Name, v1alpha1.ObjectBucketStatusPhaseBound, err)
 	}
 
 	// update OBC
 	obc.Spec.ObjectBucketName = ob.Name
 	obc.Spec.BucketName = bucketName
-	if obc, err = updateClaim(c.libClientset, obc, defaultRetryBaseInterval, defaultRetryTimeout); err != nil {
-		return fmt.Errorf("error updating OBC %q: %v", obcNsName, err)
+	obc, err = updateClaim(
+		c.libClientset,
+		obc,
+		defaultRetryBaseInterval,
+		defaultRetryTimeout)
+	if err != nil {
+		return fmt.Errorf("error updating OBC: %v", err)
 	}
-	if obc, err = updateObjectBucketClaimPhase(c.libClientset, obc, v1alpha1.ObjectBucketClaimStatusPhaseBound, defaultRetryBaseInterval, defaultRetryTimeout); err != nil {
-		return fmt.Errorf("error updating OBC %q's status to %q: %v", obcNsName, v1alpha1.ObjectBucketClaimStatusPhaseBound, err)
+	obc, err = updateObjectBucketClaimPhase(
+		c.libClientset,
+		obc,
+		v1alpha1.ObjectBucketClaimStatusPhaseBound,
+		defaultRetryBaseInterval,
+		defaultRetryTimeout)
+	if err != nil {
+		return fmt.Errorf("error updating OBC %q's status to: %v", v1alpha1.ObjectBucketClaimStatusPhaseBound, err)
 	}
 
 	log.Info("provisioning succeeded")
@@ -464,21 +513,24 @@ func (c *Controller) deleteResources(ob *v1alpha1.ObjectBucket, cm *corev1.Confi
 }
 
 // Add a finalizer to the OBC so that resource deletion can be orchestrated.
-func (c *Controller) protectOBC(obc *v1alpha1.ObjectBucketClaim) (err error) {
+// Add custom labels to the OBC.
+func (c *Controller) setOBCMetaFields(obc *v1alpha1.ObjectBucketClaim) (err error) {
 
 	clib := c.libClientset
-	obcNsName := obc.Namespace + "/" + obc.Name
 
+	logD.Info("getting OBC to set metadata fields")
 	obc, err = clib.ObjectbucketV1alpha1().ObjectBucketClaims(obc.Namespace).Get(obc.Name, metav1.GetOptions{})
 	if err != nil {
-		return fmt.Errorf("unable to Get obc %q for protection: %v", obcNsName, err)
+		return fmt.Errorf("error getting unconfigured obc: %v", err)
 	}
 
-	logD.Info("adding obc finalizer")
 	obc.SetFinalizers([]string{finalizer})
-	obc, err = clib.ObjectbucketV1alpha1().ObjectBucketClaims(obc.Namespace).Update(obc)
+	obc.SetLabels(c.provisionerLabels)
+
+	logD.Info("updating OBC metadata")
+	obc, err = updateClaim(clib, obc, defaultRetryBaseInterval, defaultRetryTimeout)
 	if err != nil {
-		return fmt.Errorf("unable to Update obc %q for protection: %v", obcNsName, err)
+		return fmt.Errorf("error configuring obc metadata: %v", err)
 	}
 
 	return nil
